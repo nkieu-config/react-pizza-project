3ff645fdecba8e0aefc1d606152d6ad889e50acf
nkieu-config <natthachak.config@gmail.com>
2025-06-30 23:35:44 +0700

    refactor: split components into separate files

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>Fist React Pizza Co.</h1>
+    </header>
+  );
+}
+export default Header;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,29 @@
+import Pizza from "./Pizza";
+import pizzaData from "../data/pizzaData";
+
+function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
+  return (
+    <main className="menu">
+      <h2>Our menu</h2>
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Authentic Italian cuisine. {numPizzas} creative dishes to choose
+            from. All from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+      )}
+    </main>
+  );
+}
+export default Menu;
